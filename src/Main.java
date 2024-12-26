f3bb103b062593698d747ec0d5ac133b1360ac67
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-12-26 13:53:17 -0300

    feat: Screen Match

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Bienvenido a ScreenMatch");
+        System.out.println("Película: Matrix");
+        int fechaDeLanzamiento = 1999;
+        System.out.println(fechaDeLanzamiento);
     }
 }
