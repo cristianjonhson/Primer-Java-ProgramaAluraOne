5aa61332c8cd151da84754f0fff2a37b217caba1
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-12-26 21:00:40 -0300

    feat: tipo de casteo

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,38 @@ public class Main {
         boolean incluidoPlan = true;
         String incluidoEnPlan = incluidoPlan ? "Sí" : "No"; // Usamos el operador ternario
         System.out.println("¿Está incluido en el plan? " + incluidoEnPlan);
+
+        String contrasena= "12345";
+        if (contrasena.equals("12345")) {
+            System.out.println("Acceso autorizado!");
+        } else {
+            System.out.println("Contraseña incorrecta.");
+        }
+
+        String nombre = "Maria";
+        int edad = 30;
+        double valor = 55.9999;
+        System.out.printf("Mi nombre es %s, tengo %d años y hoy gasté %.2f dolares%n", nombre, edad, valor);
+
+        String nombrej = "Cris";
+        int aulas = 4;
+
+        String mensaje= """
+                  Hola, %s!
+                  Bienvenido al curso de Java.
+                  Tendremos %d aulas para mostrarte lo que es necesario para que puedas dar tu primeros pasos en este lenguaje""".formatted(nombrej, aulas);
+
+        System.out.println(mensaje);
+
+        int x = 10;
+        double y = x; // casting implícito
+        System.out.println(y);
+
+
+        double xx = 10.5;
+        int yy = (int) xx; // casting explícito
+        System.out.println(yy);
+
     }
 
     private static String getString() {
